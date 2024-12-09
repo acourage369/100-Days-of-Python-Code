5309abf94cebe64875740b0c777456b41d78f5d7
Courage Adzewoda <acourageyawson369@gmail.com>
2024-12-09 00:38:58 +0000

    day 36 end

--- Day-36/stock-news-normal-start/main.py
+++ Day-36/stock-news-normal-start/main.py
@@ -1,4 +1,5 @@
 import requests
+from twilio.rest import Client
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -7,7 +8,9 @@ STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 STOCK_API_KEY = "S263JZ31EQGP4BU2"
-NEWS_API_KEY = ""
+NEWS_API_KEY = "989edd3fcca44dc7b0e290ecc94e7cfb"
+ACCOUNT_SID = "AC60f81974f8ce1730b21f41af40128476"
+AUTH_TOKEN = "3b7bb5314ec6e8ad3563f5a9f0c4f746"
 
     ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
@@ -36,14 +39,18 @@ print(day_before_yesterday_closing_price)
 
 # Find the positive difference between yesterday_closing price and day_before_yesterday_closing_price.
 difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
-print(difference)
+up_down = None
+if difference > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
 
 # percentage difference in price between closing price yesterday and closing price the day before yesterday.
-diff_percent = (difference / float(yesterday_closing_price)) * 100
+diff_percent = round((difference / float(yesterday_closing_price)) * 100)
 print(diff_percent)
 
 # Using the News API to get articles related to the COMPANY_NAME.
-if diff_percent > 5:
+if abs(diff_percent) < 5:
     new_params = {
         "apiKey": NEWS_API_KEY,
         "qInTitle": COMPANY_NAME,
@@ -52,28 +59,27 @@ if diff_percent > 5:
     news_response = requests.get(url=NEWS_ENDPOINT, params=new_params)
     news_response.raise_for_status()
     articles = news_response.json()["articles"]
-    print(articles)
 
-    #  Creating a list using slice operator that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+    #  Creating a list using slice operator that contains the first 3 articles.
     three_articles = articles[:3]
     print(three_articles)
 
 
 
- # Create a new list of the first 3 article's headline and description using list comprehension.
+    # Create a new list of the first 3 article's headline and description using list comprehension.
+    formatted_articles = [f"{STOCK_NAME}: {up_down}{diff_percent}% \nHeadline: {article['title']}, \nBrief: {article['description']}" for article in three_articles]
+    print(formatted_articles)
 
-# O 9. - Send each article as a separate message via Twilio.
+    # Send each article as a separate message via Twilio.
+    client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
+    for article in formatted_articles:
+        message = client.messages.create(
+            body=article,
+            from_="+17752615744",
+            to="+233552233328",
+        )
 
+        print(message.sid)
 
-# OptODO: Format the message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
 
