629994efce722f15801f7ced8ecce61fe44be19e
Courage Adzewoda <acourageyawson369@gmail.com>
2024-12-11 23:54:34 +0000

    day 37

--- Day-37/habit-tracker/main.py
+++ Day-37/habit-tracker/main.py
@@ -1,13 +1,29 @@
 import requests
 
-pixela_endpoint = "https://pixe.la/v1/users"
+USERNAME = "brainy"
+TOKEN = "weeewradfddfdr"
 
+pixela_endpoint = "https://pixe.la/v1/users"
+#
 user_params = {
-    "token": "weeewradfddfdr",
-    "username": "brainy",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
+#
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
 
-response = requests.post(url=pixela_endpoint, json=user_params)
+graph_config = {
+    "id": "graph1",
+    "name": "Coding Graph",
+    "unit": "km",
+    "type": "float",
+    "color": "momiji",
+}
+
+graph_endpoint = f"/v1/{pixela_endpoint}/{USERNAME}/graphs"
+
+response = requests.post(url=graph_endpoint, json=graph_config)
 print(response.text)
