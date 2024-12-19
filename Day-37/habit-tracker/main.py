e671bbc2fad06ece36bd159c93206b60b4bc07c5
Courage Adzewoda <acourageyawson369@gmail.com>
2024-12-19 13:04:30 +0000

    day 37

--- Day-37/habit-tracker/main.py
+++ Day-37/habit-tracker/main.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+
 import requests
 
 USERNAME = "brainy"
 TOKEN = "weeewradfddfdr"
+GRAPH_ID = "graph1"
 
 pixela_endpoint = "https://pixe.la/v1/users"
 #
@@ -11,19 +14,37 @@ user_params = {
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
-#
+# CREATING A USER ON PIXELA
 # response = requests.post(url=pixela_endpoint, json=user_params)
 # print(response.text)
 
-graph_config = {
-    "id": "graph1",
-    "name": "Coding Graph",
-    "unit": "km",
-    "type": "float",
-    "color": "momiji",
+# CREATING A GRAPH
+# graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+#
+# graph_config = {
+#     "id": GRAPH_ID,
+#     "name": "Coding Graph",
+#     "unit": "km",
+#     "type": "float",
+#     "color": "momiji",
+# }
+#
+headers = {
+    "X-USER-TOKEN" : TOKEN
 }
 
-graph_endpoint = f"/v1/{pixela_endpoint}/{USERNAME}/graphs"
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
 
-response = requests.post(url=graph_endpoint, json=graph_config)
+# UPDATE A PIXEL TO GRAPH ON PIXELA
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime.now()
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "10.20",
+}
+#
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
 print(response.text)
